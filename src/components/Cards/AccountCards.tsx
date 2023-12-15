bff1e667967f2884ab6e73fc12debb0c44f43d46
bukanEngineer <heispartofmyworks@gmail.com>
2023-12-15 15:00:34 +0700

    After separate metadata and card container

--- src/components/Cards/AccountCards.tsx
+++ src/components/Cards/AccountCards.tsx
@@ -1,7 +1,27 @@
+import React from "react";
 import { Card, Divider } from "antd";
 import CustomCard from "./CustomCard";
 
 const { Meta } = Card;
+
+const AccountSection: React.FC<{ title: string; description: string; data: any[]; isLast: boolean}> = ({
+  title,
+  description,
+  data,
+  isLast,
+}) => (
+  <>
+    <Meta title={title} description={description} style={{ paddingBottom: "16px" }} />
+
+    <div style={{ gap: "16px", display: "flex", flexDirection: "column" }}>
+      {data.map((card, index) => (
+        <CustomCard key={index} {...card} />
+      ))}
+    </div>
+
+    {!isLast && <Divider />}
+  </>
+);
 
 const AccountCards: React.FC = () => {
   const cashAccountData = [
@@ -13,7 +33,6 @@ const AccountCards: React.FC = () => {
       accountType: "Virtual Account",
       totalMoney: "Rp.20.312.3123,00",
     },
-
     {
       link: "https://www.youtube.com/",
       avatarSrc: "https://www.worldometers.info/img/flags/my-flag.gif",
@@ -28,8 +47,7 @@ const AccountCards: React.FC = () => {
     {
       link: "#",
       title: "IDR Account",
-      description:
-        "Grow your money with deposit from IDR 10.000.000 up to IDR 100.000.000",
+      description: "Grow your money with deposit from IDR 10.000.000 up to IDR 100.000.000",
     },
   ];
 
@@ -37,57 +55,27 @@ const AccountCards: React.FC = () => {
     {
       link: "#",
       title: "IDR Account",
-      description:
-        "Grow your money with deposit from IDR 10.000.000 up to IDR 100.000.000",
+      description: "Grow your money with deposit from IDR 10.000.000 up to IDR 100.000.000",
       buttonText: "Explore Sekarang",
       buttonLink: "#",
     },
   ];
 
+  const sections = [
+    { title: "Cash Account", description: "For accepting or sending payment", data: cashAccountData },
+    { title: "Earn Account", description: "Access and manage your savings account here", data: earnAccountData },
+    {
+      title: "Loan Account",
+      description: "Access and manage your Loan here",
+      data: [...cashAccountData, ...flexPayData.map((card) => ({ ...card, showChevron: false }))],
+    },
+  ];
+
   return (
     <Card title="Account" style={{ width: "100%", height: "100%" }}>
-      <Meta
-        title="Cash Account"
-        description="For accepting or sending payment"
-        style={{ paddingBottom: "16px" }}
-      />
-
-      <div style={{ gap: "16px", display: "flex", flexDirection: "column" }}>
-        {cashAccountData.map((card, index) => (
-          <CustomCard key={index} {...card} />
-        ))}
-      </div>
-
-      <Divider />
-      <Meta
-        title="Earn Account"
-        description="Access and manage your savings account here"
-        style={{ paddingBottom: "16px" }}
-      />
-
-      <div style={{ gap: "16px", display: "flex", flexDirection: "column" }}>
-        {earnAccountData.map((card, index) => (
-          <CustomCard key={index} {...card} />
-        ))}
-      </div>
-
-      <Divider />
-
-      <Meta
-        title="Loan Account"
-        description="Access and manage your Loan here"
-        style={{ paddingBottom: "16px" }}
-      />
-
-      <div style={{ gap: "16px", display: "flex", flexDirection: "column" }}>
-        {cashAccountData.map((card, index) => (
-          <CustomCard key={index} {...card} />
-        ))}
-
-        {flexPayData.map((card, index) => (
-          <CustomCard key={index} {...card} showChevron={false} />
-        ))}
-      </div>
+      {sections.map((section, index) => (
+        <AccountSection key={index} {...section} isLast={index === sections.length - 1} />
+      ))}
     </Card>
   );
 };
